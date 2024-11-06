0417d21d397da73d3c553eb3b99a87435f02c112
emmach02 <emmanuelricardochaile@hotmail.com>
2024-11-05 22:14:19 -0300

    primer commit

---
+++ backendG16/src/test/java/com/backend3K6_2024/backendG16/BackendG16ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend3K6_2024.backendG16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendG16ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
