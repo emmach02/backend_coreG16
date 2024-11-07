89ce2c8ad28d011df8fe29bfaec82efae73e6dd3
amdresuli <andresblanco629@gmail.com>
2024-11-06 21:30:02 -0300

    vehiculos agregadoo

--- backendG16/src/main/java/com/backend3K6_2024/backendG16/Interesados/repository/InteresadoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.backend3K6_2024.backendG16.Interesados.repository;
-
-
-import com.backend3K6_2024.backendG16.Interesados.entity.Interesado;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InteresadoRepository extends JpaRepository<Interesado, Integer> {
-}
